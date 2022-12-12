41f6c22a988df0b14ac4884eb450b7778f3f93e9
Iris Pascual <irispblg@gmail.com>
2022-12-12 15:21:35 +0100

    add Dashboard

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,9 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Dashboard from "../views/Dashboard.vue"
 
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes: [{
+    path: "/", component: Dashboard
+
+  }
+
+  ]
 })
 
 export default router
