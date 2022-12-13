b6f03759ccca7174e088458ead530b3d92bc2963
Iris Pascual <irispblg@gmail.com>
2022-12-13 10:16:38 +0100

    add new components

--- vue-project/src/components/Nav.vue
+++ vue-project/src/components/Nav.vue
@@ -1,7 +1,13 @@
 <template>
+  <div>
     <div>
-        
+      <img src="../../public/logo.png" alt="logo-tool" />
     </div>
+    <ul>
+      <li>Log In</li>
+      <li>Sign Up</li>
+    </ul>
+  </div>
 </template>
 
 <script>
--- vue-project/src/views/Dashboard.vue
+++ vue-project/src/views/Dashboard.vue
@@ -1,12 +1,18 @@
 <template>
-  <div></div>
+ <Nav />
+ 
 </template>
 
 <script>
 import { mapStores } from "pinia";
 import userStore from "../stores/user.js";
+import Nav from "../components/Nav.vue";
+
 
 export default {
+  components: {
+    Nav,
+  },
   computed: {
     ...mapStores(userStore),
   },
