1553b951157bc3629d426018644a31a050219a9f
ale <ale93chacon@gmail.com>
2022-12-13 11:36:23 +0100

    signup

--- vue-project/src/views/Dashboard.vue
+++ vue-project/src/views/Dashboard.vue
@@ -1,6 +1,7 @@
 <template>
  <Nav />
  <Login />
+ <SignUp />
  
 </template>
 
@@ -9,12 +10,14 @@ import { mapStores } from "pinia";
 import userStore from "../stores/user.js";
 import Nav from "../components/Nav.vue";
 import Login from "../components/Login.vue";
+import SignUp from "../components/SignUp.vue";
 
 
 export default {
   components: {
     Nav,
     Login,
+    SignUp,
   },
   computed: {
     ...mapStores(userStore),
