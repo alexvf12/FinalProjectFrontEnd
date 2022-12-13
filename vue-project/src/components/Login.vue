1cb796552c477dbf4d9b57b81c419d753f40102d
Iris Pascual <irispblg@gmail.com>
2022-12-13 10:44:20 +0100

    add form logIn

---
+++ vue-project/src/components/Login.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>Log In</h1>
+ <form name="myForm" id="form" action="" onsubmit="" method="post">
+ <div>
+ <label for="">USER</label>
+ <input type="text" placeholder="Enter your email" name="fname" id="">
+ </div>
+ <div>
+ <label form="">PASSWORD</label>
+ <input type="text" placeholder="Enter your password" name="fpassword" id="password">
+ 
+ </div>
+ <input type="checkbox">Remember me 
+ <button>LOG IN</button>
+ 
+ </form>
+  
+</template>
--- vue-project/src/views/Dashboard.vue
+++ vue-project/src/views/Dashboard.vue
@@ -1,5 +1,6 @@
 <template>
  <Nav />
+ <Login />
  
 </template>
 
@@ -7,11 +8,13 @@
 import { mapStores } from "pinia";
 import userStore from "../stores/user.js";
 import Nav from "../components/Nav.vue";
+import Login from "../components/Login.vue";
 
 
 export default {
   components: {
     Nav,
+    Login,
   },
   computed: {
     ...mapStores(userStore),
