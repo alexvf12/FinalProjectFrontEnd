e446f6fd9e63b7a2082360c6011b7264b52986e2
Alex Virgili <alexvf12@gmail.com>
2022-12-14 12:21:26 +0100

    sign

--- vue-project/src/components/Login.vue
+++ vue-project/src/components/Login.vue
@@ -1,26 +1,41 @@
 <template>
     <div class="">
-        <h1>Log In</h1>
-        <form @submit.prevent="login" name="myForm" id="form" action="" onsubmit="" method="post">
-            <div>
-                <label for="">USER</label>
-                <input v-model="email" type="text" placeholder="Enter your email" name="fname" id="" />
-            </div>
-            <div>
-                <a href="">Forgot password?</a>
-                <label form="">PASSWORD</label>
-                <input type="text" v-model="password" placeholder="Enter your password" name="fpassword"
-                    id="password" />
-            </div>
-            <input type="checkbox" />Remember me
-            <button>LOG IN</button>
-        </form>
 
-        <hr />
-        <p>No account?</p>
-        <a href="">Sign up for free</a>
+        <form @submit.prevent="login()" class="screen-1 signup" name="myForm" action="" onsubmit="" method="post">
+            <div class="d-flex justify-content-center">
+                <img class="w-75 mb-4" src="../../public/logo.png" alt="logo-tool" />
+            </div>
+            <h4 class="">LOG IN</h4>
+            <div class="email">
+                <label for="email">Email Address</label>
+                <div>
+                    <ion-icon name="mail-outline"></ion-icon>
+                    <input v-model="email" type="email" name="email" placeholder=" Username@gmail.com" />
+                </div>
+            </div>
+            <div class="password">
+                <div class="d-flex justify-content-between">
+                    <label for="password">Password</label>
+                    <a class="size-xs forgot" href="">Forgot password?</a>
+                </div>
+                <div>
+                    <ion-icon name="lock-closed-outline"></ion-icon>
+                    <input v-model="password" class="pas" type="password" name="password" placeholder=" ············" />
+                    <ion-icon class="show-hide" name="eye-outline"></ion-icon>
+                </div>
+            </div>
+            <div class="d-flex">
+                <input class="mb-3 mx-2" type="checkbox" />
+                <span class="mb-3">Remember me.</span>
+            </div>
+            <button class="login w-50 align-self-center">Log In</button>
+
+            <div class="footer"><span>Signup</span><span>Forgot Password?</span></div>
+        </form>
     </div>
+
 </template>
+
 <script>
 import { mapStores } from "pinia";
 import userStore from "../stores/user.js";
@@ -42,3 +57,121 @@ export default {
     },
 };
 </script>
+
+<style scoped>
+* {
+    font-family: "Poppins";
+}
+
+.signup {
+    display: flex;
+    background: #dde5f4;
+    height: 100vh;
+}
+
+.screen-1 {
+    background: #f1f7fe;
+    padding: 2em;
+    display: flex;
+    flex-direction: column;
+    border-radius: 30px;
+    box-shadow: 0 0 2em #e6e9f9;
+    /* gap: 2em; */
+}
+
+
+.screen-1 .email {
+    background: white;
+    box-shadow: 0 0 2em #e6e9f9;
+    padding: 1em 2rem;
+    display: flex;
+    flex-direction: column;
+    gap: 0.5em;
+    border-radius: 20px;
+    color: #4d4d4d;
+    margin-bottom: 1.5rem;
+    margin-top: 0.5rem;
+}
+
+.screen-1 .email input {
+    outline: none;
+    border: none;
+    width: 85%;
+
+}
+
+.screen-1 .email ion-icon {
+    color: #4d4d4d;
+    margin-bottom: -0.2em;
+}
+
+.screen-1 .password {
+    background: white;
+    box-shadow: 0 0 2em #e6e9f9;
+    padding: 1em 2rem;
+    display: flex;
+    flex-direction: column;
+    gap: 0.5em;
+    border-radius: 20px;
+    color: #4d4d4d;
+    margin-bottom: 1.5rem;
+    margin-top: 0.3rem;
+}
+
+.screen-1 .password input {
+    outline: none;
+    border: none;
+    width: 85%;
+}
+
+.screen-1 .password ion-icon {
+    color: #4d4d4d;
+    margin-bottom: -0.1em;
+}
+
+.screen-1 .email ion-icon {
+    color: #4d4d4d;
+    margin-bottom: -0.1em;
+}
+
+.screen-1 .login {
+    padding: 1em;
+    background: #3e4684;
+    color: white;
+    border: none;
+    border-radius: 30px;
+    font-weight: 600;
+}
+
+.screen-1 .footer {
+    display: flex;
+    font-size: 0.7em;
+    color: #5e5e5e;
+    justify-content: space-between;
+}
+
+.screen-1 .footer span {
+    cursor: pointer;
+}
+
+button {
+    cursor: pointer;
+    margin-bottom: 1rem;
+}
+
+.email {
+    display: flex;
+    justify-content: flex-start;
+}
+
+h4 {
+    font-size: 1.2rem;
+    margin-left: 0.5rem;
+}
+
+.forgot {
+    font-size: x-small;
+    margin-top: 0.3rem;
+    text-decoration: none;
+}
+</style>
--- vue-project/src/components/SignUp.vue
+++ vue-project/src/components/SignUp.vue
@@ -1,25 +1,40 @@
 <template>
-   <div>
-      <h1>Sign up for FREE</h1>
-      <form @submit.prevent="signup()" name="myForm" action="" onsubmit="" method="post">
-         <div>
-            <label for="">FULL NAME</label>
-            <input v-model="fullname" type="text" placeholder=" " name="fname">
+   <div class="">
+
+      <form @submit.prevent="signup()" class="screen-1 signup" name="myForm" action="" onsubmit="" method="post">
+         <div class="d-flex justify-content-center">
+            <img class="w-75 mb-4" src="../../public/logo.png" alt="logo-tool"/>
          </div>
-         <div>
-            <label for="">EMAIL</label>
-            <input v-model="email" type="email" placeholder=" " name="femail">
+         <h4 class="">SING UP FOR FREE</h4>
+         <div class="email">
+            <label for="name">Full name</label>
+            <div>
+               <ion-icon name="mail-outline"></ion-icon>
+               <input v-model="fullname" type="text" placeholder=" Username" name="name">
+            </div>
          </div>
-         <div>
-            <label form="">PASSWORD</label>
-            <input v-model="password" type="password" placeholder="" name="fpassword">
+         <div class="email">
+            <label for="email">Email Address</label>
+            <div>
+               <ion-icon name="mail-outline"></ion-icon>
+               <input v-model="email" type="email" name="email" placeholder=" Username@gmail.com" />
+            </div>
          </div>
-         <input type="checkbox">I agree to the <span>Terms of Service</span> and <span>Privacy Policy</span>
-         <button>CREATE ACCOUNT</button>
+         <div class="password">
+            <label for="password">Password</label>
+            <div>
+               <ion-icon name="lock-closed-outline"></ion-icon>
+               <input v-model="password" class="pas" type="password" name="password" placeholder=" ············" />
+               <ion-icon class="show-hide" name="eye-outline"></ion-icon>
+            </div>
+         </div>
+         <div class="d-flex">
+                <input class="mb-3 mx-2" type="checkbox" />
+                <span class="mb-3 size-xs">I agree to the <a href="">Terms of Service</a> and <a href="">Privacity Policy</a> .</span>
+            </div>
+         <button class="login w-50 align-self-center">Create account </button>
+         <div class="footer"><span>Signup</span><span>Forgot Password?</span></div>
       </form>
-      <hr>
-      <p>Already have an account?</p>
-      <a href="">Login</a>
    </div>
 
 </template>
@@ -46,3 +61,119 @@ export default {
    },
 };
 </script>
+
+<style scoped>
+* {
+   font-family: "Poppins";
+}
+
+.signup {
+   display: flex;
+   background: #dde5f4;
+   height: 100vh;
+}
+
+.screen-1 {
+   background: #f1f7fe;
+   padding: 2em;
+   display: flex;
+   flex-direction: column;
+   border-radius: 30px;
+   box-shadow: 0 0 2em #e6e9f9;
+   /* gap: 2em; */
+}
+
+
+.screen-1 .email {
+   background: white;
+   box-shadow: 0 0 2em #e6e9f9;
+   padding: 1em 2rem;
+   display: flex;
+   flex-direction: column;
+   gap: 0.5em;
+   border-radius: 20px;
+   color: #4d4d4d;
+   margin-bottom: 1.5rem;
+   margin-top: 0.5rem;
+}
+
+.screen-1 .email input {
+   outline: none;
+   border: none;
+   width: 85%;
+
+}
+
+.screen-1 .email ion-icon {
+   color: #4d4d4d;
+   margin-bottom: -0.2em;
+}
+
+.screen-1 .password {
+   background: white;
+   box-shadow: 0 0 2em #e6e9f9;
+   padding: 1em 2rem;
+   display: flex;
+   flex-direction: column;
+   gap: 0.5em;
+   border-radius: 20px;
+   color: #4d4d4d;
+   margin-bottom: 1.5rem;
+   margin-top: 0.3rem;
+}
+
+.screen-1 .password input {
+   outline: none;
+   border: none;
+   width: 85%;
+}
+
+.screen-1 .password ion-icon {
+   color: #4d4d4d;
+   margin-bottom: -0.1em;
+}
+
+.screen-1 .email ion-icon {
+   color: #4d4d4d;
+   margin-bottom: -0.1em;
+}
+
+.screen-1 .login {
+   padding: 1em;
+   background: #3e4684;
+   color: white;
+   border: none;
+   border-radius: 30px;
+   font-weight: 600;
+}
+
+.screen-1 .footer {
+   display: flex;
+   font-size: 0.7em;
+   color: #5e5e5e;
+   justify-content: space-between;
+}
+
+.screen-1 .footer span {
+   cursor: pointer;
+}
+
+button {
+   cursor: pointer;
+   margin-bottom: 1rem;
+}
+
+.email {
+   display: flex;
+   justify-content: flex-start;
+}
+
+h4{
+   font-size: 1.2rem;
+   margin-left: 0.5rem;
+}
+
+.size-xs{
+   font-size: x-small;
+}
+</style>
