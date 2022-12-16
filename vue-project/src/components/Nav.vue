1bdc40dc6327e0a0925cc55563bfcf5ddc3af286
Iris Pascual <irispblg@gmail.com>
2022-12-16 16:17:16 +0100

    style modif

--- vue-project/src/components/Nav.vue
+++ vue-project/src/components/Nav.vue
@@ -1,10 +1,10 @@
 <template>
   <nav>
     <div class="d-flex justify-content-between">
-      <img src="../../public/logo.png" alt="logo-tool" width="170" />
+      <img src="../../public/logo.png" alt="logo-tool" class="w-50"  />
 
       <div class="ul">
-        <ul class="d-flex flex-row align-items-center list-unstyled ">
+        <ul class="d-flex list-unstyled ">
           <li><button v-if="!userStore.user" @click="$emit('add2')" type="button" class="btn btn-outline-warning btn-sm">Log In</button></li>
           <li><button v-if="!userStore.user" @click="$emit('add')" type="button" class="btn btn-outline-warning btn-sm">Sign Up</button></li>
           <li><button v-if="userStore.user" @click="isLoggedOut()" type="button" class="btn btn-outline-warning btn-sm">Log Out</button></li>
